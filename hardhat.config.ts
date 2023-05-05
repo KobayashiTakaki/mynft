fc47ae28bf20371d5e1e72cb7d8bceb0e299ec48
kobayashitakaki <o.share.thief@gmail.com>
2023-05-05 14:34:08 +0900

    implement NFT

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,30 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+import * as dotenv from 'dotenv'
+dotenv.config();
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: "0.8.17",
+  networks: {
+    localhost: {
+      url:"http://127.0.0.1:8545/",
+      chainId:31337,
+    },
+    shibuya: {
+      url: "https://evm.shibuya.astar.network",
+      chainId:81,
+      accounts: [
+        `0x${process.env.PRIVATE_KEY}`,
+      ],
+    },
+    mumbai: {
+      url: "https://matic-mumbai.chainstacklabs.com",
+      accounts: [
+        `0x${process.env.PRIVATE_KEY}`,
+      ],
+    },
+  }
 };
 
 export default config;
